21d8e0d72b596b7d5a9db91b262e84c39d8f68d6
tauhidul35 <tauhidul35@gmail.com>
2015-08-15 02:19:39 +0600

    Initiat MVC 5 web project

---
+++ tutorsbd/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace tutorsbd
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ tutorsbd/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="tutorsbd.MvcApplication" Language="C#" %>
